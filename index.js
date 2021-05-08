ec3032868d1fe18779a63bbb303b8c9a9e8c42c6
Katja Sarain <katjasarain@gmail.com>
2021-05-08 12:27:09 -0700

    wrote questions and input/output works

---
+++ index.js
@@ -0,0 +1,48 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is name of your README?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Describe your README',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How do you install this program?',
+    },
+    {
+        type: 'input',
+        name: 'use',
+        message: 'How do you use this program?',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines?',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What are the testing guidelines?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What is the license?',
+      },
+      {
+        type: 'input',
+        name: 'contact',
+        message: 'What is your GitHub and email?',
+      },
+  ])
+  .then((response) =>
+    console.log(response));
+    fs.writeFile('READMEpop.md', generateMarkdown(response), (err) => err ? console.log(err): console.log('Success'));
